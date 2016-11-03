15129d4420e2762a16d5531f39a883ce23e7797f
Aditya Ayyakad <njadtiya@gmail.com>
2016-11-03 11:41:04 -0400

    Brush size slider works

--- app/src/main/java/com/cmsc434/adi/doodle/MainActivity.java
+++ app/src/main/java/com/cmsc434/adi/doodle/MainActivity.java
@@ -2,6 +2,7 @@ package com.cmsc434.adi.doodle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupBrushSizeSeekBar();
+    }
+
+    private void setupBrushSizeSeekBar() {
+        ScribbleView scribbleView = (ScribbleView) findViewById(R.id.scribble);
+        SeekBar brushSizeSeekBar = (SeekBar) findViewById(R.id.brush_seek_bar);
+
+        brushSizeSeekBar.setMax(getResources().getInteger(R.integer.largest_size));
+        brushSizeSeekBar.setProgress(getResources().getInteger(R.integer.initial_size));
+        brushSizeSeekBar.setOnSeekBarChangeListener(scribbleView);
     }
 
 }
